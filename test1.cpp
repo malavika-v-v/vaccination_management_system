6f88fb889a20f8e16f56a7bd2395bf54c0cc6cfe
malavika-v-v <malavika.vv@teamta.in>
2021-08-18 12:46:17 +0530

    code refactor

--- test1.cpp
+++ test1.cpp
@@ -25,7 +25,7 @@
             cin >> pincode;
             cout << "Number of new Cowin vaccine : ";
             cin >> cowinDosesTotal;
-            cout << "Number of new Covishiled vaccine : ";
+            cout << "Number of new Covishield vaccine : ";
             cin >> covishieldDosesTotal;
             total_vaccine_number = cowinDosesTotal + covishieldDosesTotal;
         }
